40c1824cbff61099b8acd13258177fb6e1e17bcd
DariaLaz <daria.lazarova03@gmail.com>
2023-01-07 14:08:07 +0200

    Key Expansion

--- Crypting/AdvancedEncryptionStandard/AdvancedEncryptionStandard.cpp
+++ Crypting/AdvancedEncryptionStandard/AdvancedEncryptionStandard.cpp
@@ -1,20 +1,65 @@
 ﻿#include <iostream>
 
-//RijndaelKeyShedule
-
-char*** EncryptRijndaelKeyShedule(char key[4][4]) {
-    for (size_t row = 0; row < 4; row++)
+//RIJNDAEL KEY SHEDULE/EXPANSION
+//Rot Word
+void Rotate(char* row) {
+    char first = row[0];
+    for (size_t i = 0; i < 3; i++)
     {
-        for (size_t col = 0; col < 4; col++)
-        {
-
-        }
+        row[i] = row[i + 1];
+    }
+    row[3] = first;
+    return;
+}
+//Sub Word
+void SubstituteSBox(char &element) {
+    element = element 
+              ^ (element < 1) 
+              ^ (element < 2)
+              ^ (element < 3)
+              ^ (element < 4)
+              ^ 99;
+}
+//Round constants
+void RoundConstantUpdate(int& c) {
+    if (c < 128)
+    {
+        c *= 2;
+    }
+    else
+    {
+        c = (2 * c) ^ 283;
     }
 }
 
-void Encrypt(char text[4][4], char key[4][4]) {
+void EncryptRijndaelKeyShedule(char key[4][4]) {
+    int rountConstant = 1;
+
+    for (size_t row = 0; row < 4; row++)
+    {
+        Rotate(key[row]);
+        key[row][0] ^= rountConstant;
+        RoundConstantUpdate(rountConstant);
+        for (size_t col = 0; col < 4; col++)
+        {
+            SubstituteSBox(key[row][col]);
+        }
+    }
+}
+void EncryptCurrentMatrix(char text[4][4], char key[4][4]) {
     char result = ' ';
-    char*** keyMatrix = EncryptRijndaelKeyShedule(key);
+
+    //2. Key Expansion
+    EncryptRijndaelKeyShedule(key);
+    //3. Add Round Key
+
+
+    for (size_t i = 0; i < 9; i++)
+    {
+        
+        //
+    }
+    //char*** keyMatrix = EncryptRijndaelKeyShedule(key);
     std::cout << result;
 }
 //void Decrypt(char* text) {
@@ -26,7 +71,7 @@ void Encrypt(char text[4][4], char key[4][4]) {
 
 int main()
 {
-    char text[4][4] = { 'm', 'y', ' ', 't',
+    /*char text[4][4] = { 'm', 'y', ' ', 't',
                         'e', 'x', 't', ' ',
                         'i', 's', ' ', 'n',
                         'i', 'c', 'e', '.' };
@@ -34,6 +79,6 @@ int main()
                        'a', 't', 'e', 'r',
                        'r', 'e', 's', 't',
                        'r', 'i', 'a', 'l' };
-    Encrypt(text, key);
+    Encrypt(text, key);*/
 }
 
