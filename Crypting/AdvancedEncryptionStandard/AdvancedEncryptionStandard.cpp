a818fe7af4760b2d46f6ceb77133569a4abfaef9
DariaLaz <daria.lazarova03@gmail.com>
2023-01-07 14:56:50 +0200

    Mixing columns

--- Crypting/AdvancedEncryptionStandard/AdvancedEncryptionStandard.cpp
+++ Crypting/AdvancedEncryptionStandard/AdvancedEncryptionStandard.cpp
@@ -70,23 +70,62 @@ void ByteSubstitution(char text[4][4]) {
     }
 }
 
-void EncryptCurrentMatrix(char text[4][4], char key[4][4]) {
+//SHIFT ROWS
+void ShiftElements(char row[4], int count) {
+    for (size_t i = 0; i < count; i++)
+    {
+        char first = row[0];
+        for (size_t i = 0; i < 3; i++)
+        {
+            row[i] = row[i + 1];
+        }
+        row[3] = first;
+    }
+}
+void ShiftRows(char text[4][4]) {
+    for (size_t row = 0; row < 4; row++)
+    {
+        ShiftElements(text[row], row);
+    }
+}
+
+//MIXING COLUMNS
+void MatrixMultiplication(char text[4][4], int keyMatrix[4][4]) {
+    for (size_t row = 0; row < 4; row++)
+    {
+        for (size_t col = 0; col < 4; col++)
+        {
+            int currentValue = 0;
+            for (size_t i = 0; i < 4; i++)
+            {
+                currentValue += text[row][i] * keyMatrix[i][col];
+            }
+            text[row][col] = currentValue;
+        }
+    }
+}
+
+void EncryptCurrentMatrix(char text[4][4], char key[4][4], int keyMatrix[4][4]) {
     char result = ' ';
 
-    
     //3. Add Round Key
     AddRoundKey(key, text);
     //2. Key Expansion
     EncryptRijndaelKeyShedule(key);
 
-
     for (size_t i = 0; i < 9; i++)
     {
         //4. Byte substitution
         ByteSubstitution(text);
-        //
+        //5. Shift Rows
+        ShiftRows(text);
+        //6. Mixing columns
+        MatrixMultiplication(text, keyMatrix);
+        //3. Add Round Key
+        AddRoundKey(key, text);
+        //2. Key Expansion
+        EncryptRijndaelKeyShedule(key);
     }
-    //char*** keyMatrix = EncryptRijndaelKeyShedule(key);
     std::cout << result;
 }
 //void Decrypt(char* text) {
@@ -107,7 +146,7 @@ void PrintMatrix(char arr[4][4]) {
 }
 int main()
 {
-    /*char text[4][4] = { 'm', 'y', ' ', 't',
+    char text[4][4] = { 'm', 'y', ' ', 't',
                         'e', 'x', 't', ' ',
                         'i', 's', ' ', 'n',
                         'i', 'c', 'e', '.' };
@@ -115,5 +154,11 @@ int main()
                        'a', 't', 'e', 'r',
                        'r', 'e', 's', 't',
                        'r', 'i', 'a', 'l' };
-    Encrypt(text, key);*/
+    char keyMatrix[4][4] = { 2, 3, 1, 1,
+                             1, 2, 3, 1,
+                             1, 1, 2, 3,
+                             3, 1, 1, 2 };
+    EncryptCurrentMatrix(text, key, keyMatrix);
+
+
 }
